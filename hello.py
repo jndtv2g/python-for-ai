31b420f51dd5d7057c6f5a3c75068237eda61ab6
jndtv2g <jvitug@ymail.com>
2026-01-18 00:50:26 +0800

    Update user_age variable and enhance greeting function output

--- hello.py
+++ hello.py
@@ -8,22 +8,41 @@ print("This is a test Python file.")
 # Testing variables
 first_name = "John"     #string
 last_name = "Doe"      #string
-user_age = 26           #int
+user_age = 17           #int
 is_logged_in = True     #boolean
 
 # Testing functions
+# Function to greet a user
 def greet(name):
     if name: # check if name is not empty
         print(f"Hello {name}!")
     else: # if name is empty
         print("Hello!")
 
-
+# Function to count characters in a string
 def count_characters(full_name):
     return len(full_name)
 
+
+def can_drive(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+    
+
+
+
 # Call the function to test as outputs
 print("\n")
-greet(first_name + " " + last_name)
+greet(first_name + " " + last_name + "!")
 print(f"Total characters in full name: {count_characters(first_name + ' ' + last_name)}")
 print("\n")
+
+# Check driving eligibility
+if can_drive(user_age):
+    print(f"{first_name} is eligible to drive.")
+    print("\n")
+else:
+    print(f"{first_name} is not eligible to drive.")
+    print("\n")
