50b819d369778c2b24482db03c18037709ae0db7
jndtv2g <jvitug@ymail.com>
2026-01-18 00:20:40 +0800

    Add variable declarations and comments to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
+import requests
+
+# Testing string outputs
 print("Hello World!")
 print("This is a test Python file.")
 
+# Testing variables
+first_name = "John"     #string
+user_age = 26           #int
+is_logged_in = True     #boolean
+
+
+# Testing functions
 def greet(name):
     if name:
         print(f"Hello {name}!")
